ad3fd2c4b1225c4806e80b369d0ad8927bab43ef
tomesterez <testerez@gmail.com>
2015-07-03 14:23:36 -0400

    add demo

--- typescript/demo.ts
+++ typescript/demo.ts
@@ -4,3 +4,4 @@ $('.tooltip1').tooltip();
 $('.tooltip2').tooltip({cssClass:'tooltip-dark'});
 $('.tooltip3').tooltip({cssClass:'tooltip-pill'});
 $('.tooltip4').tooltip({source:Tooltip.Source.href});
+$('.tooltip5').tooltip({showOn:Tooltip.ShowOn.click});
