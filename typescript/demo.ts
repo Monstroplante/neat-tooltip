02a48ced6fa8b65302a67338645f34800d2ae086
tomesterez <testerez@gmail.com>
2015-07-04 14:32:48 -0400

    fix responsive

--- typescript/demo.ts
+++ typescript/demo.ts
@@ -4,7 +4,7 @@ $('.tooltip1').tooltip();
 $('.tooltip1top').tooltip({ position:'top'});
 $('.tooltip2').tooltip({ cssClass:'tooltip-dark'});
 $('.tooltip3').tooltip({ cssClass: 'tooltip-pill' });
-$('.tooltip4').tooltip({ source: 'anchor' }, 'click');
+$('.tooltip4').tooltip({ source: 'anchor', cssClass: 'tooltip-large'}, 'click');
 $('.tooltip5').tooltip({ content: function () { return 'time = ' + new Date().getTime(); } });
 
 $('#demo_delegation').tooltip({}, 'hover', 'span')
